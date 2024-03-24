1f618f0bab94ab978d09eb525c972c07d3e111e5
navajyoth puthalath <106015664+navajyoth-puthalath13@users.noreply.github.com>
2024-03-24 15:38:20 +0530

    fetch infomation like no of object and object name

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import cv2 as cv
 import numpy as np
 from time import time
+import pyttsx3
 from ultralytics import YOLO
-import win32ui , win32con, win32gui
+import win32ui, win32con, win32gui
 
 x = 40  # X-coordinate of the top-left corner of the ROI
 y = 164  # Y-coordinate of the top-left corner of the ROI
 width = 1180  # Width of the ROI
 height = 665  # Height of the ROI
 
+def audio( sv,sd):
+    engine = pyttsx3.init(driverName='sapi5')
+    engine.say(f"{sd}")
 
-
-def win_capture(bbox):
+def win_capture( bbox ):
     x, y, x2, y2 = bbox
     w, h = x2 - x, y2 - y
 
@@ -55,16 +58,23 @@ while True:
     # Perform object detection
     results = model(screenshot)
 
-    # Access the first detection (assuming you want the first one)
-    for result in results:
-        probs = result.probs  # Probs object for classification outputs
+    num_objects = 0
+    class_names = []
 
-        if probs is not None:
-            # Iterate over each detection
-            for i in range(len(probs)):
-                class_id = probs[i].argmax()  # Get the class ID with the highest probability
-                class_name = model.names[class_id]  # Look up the class name using the class ID
-                print(f"Detected object: {class_name}")
+    if results is not None:  # Check if any detections were made
+        for detection in results:  # Assuming results is a list of detections
+            num_objects += 1
+
+            # Access class ID and convert it to class name using model.names
+            class_id = int(detection.boxes.cls[0])  # Assuming class is the first element
+            class_name = model.names[class_id]
+
+            class_names.append(class_name)
+
+        print(f"Number of objects detected: {num_objects}")
+        print(f"Class names: {class_names}")
+
+
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
